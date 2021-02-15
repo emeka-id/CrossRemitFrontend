c20af8409a7e61a6ce443027599b2214c13025c3
Divine Philip <p.divine@truparse.com>
2021-02-15 22:28:48 +0100

    updates to the tab title

--- src/components/tabs/tab-title/index.tsx
+++ src/components/tabs/tab-title/index.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 type Props = {
   title: string
+  index: number
+  setSelectedTab: (index: number) => void
 }
 
-const TabTitle: React.FC<Props> = ({ title }) => {
+const TabTitle: React.FC<Props> = ({ title, setSelectedTab, index }) => {
+
+  const onClick = useCallback(() => {
+    setSelectedTab(index)
+  }, [setSelectedTab, index])
 
   return (
     <li>
-      <button>{title}</button>
+      <button onClick={onClick}>{title}</button>
     </li>
   )
 }
