e1312f7fc32183a5d5faf0ac22006ac1ca9684c5
Divine Philip <p.divine@truparse.com>
2021-02-15 22:28:11 +0100

    Added the tab components to the page

--- src/pages/main/settings/index.tsx
+++ src/pages/main/settings/index.tsx
@@ -1,17 +1,37 @@
-import { Button, Card, CustomInput } from 'components';
+import { Button, Card, CustomInput, Tabs } from 'components';
 import React from 'react';
 import styles from './settings.module.scss';
+import Tab from '../../../components/tabs/tab'
 
 const Settings = () => {
   return (
     <>
       Settings
       <Card>
-        Deposit Amount
+        {/* Deposit Amount
           <div className={styles.deposit}>
           <CustomInput label="Enter Amount to invest" />
         </div>
-        <Button>Deposit</Button>
+        <Button>Deposit</Button> */}
+        <Tabs>
+          <Tab title="Bank Details">
+            Hello, these are bank details
+            {/* Contents of the tab */}
+          </Tab>
+          <Tab title="Verification">
+            Hello, these are verification details
+            {/* Contents of the tab */}
+          </Tab>
+          <Tab title='Profile'>
+            Hello, these are profile details
+            {/* Contents of the tab */}
+          </Tab>
+          <Tab title="Password">
+            Hello, these are password details
+            {/* Contents of the tab */}
+          </Tab>
+        </Tabs>
+
       </Card>
     </>
   );
