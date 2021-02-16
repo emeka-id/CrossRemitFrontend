278a9fa29642ed4706d029c1fd861d6d69781fa3
Divine Philip <p.divine@truparse.com>
2021-02-16 18:05:07 +0100

    got started on the profile

--- src/pages/main/settings/index.tsx
+++ src/pages/main/settings/index.tsx
@@ -25,8 +25,39 @@ const Settings = () => {
             {/* Contents of the tab */}
           </Tab>
           <Tab title='Profile'>
-            Hello, these are profile details
-            {/* Contents of the tab */}
+            <div className={styles.profile}>
+              <div>
+                Name
+                <div>
+                  <input type="text" placeholder="First name" />
+                  <input type="text" placeholder="Last name" />
+                </div>
+                Date of Birth
+                <input type="date" name="date-of-birth" id="" />
+                Gender
+                <select name="select-gender" id="">
+                  <option value="select-gender">Select your gender</option>
+                  <option value="gender-male">Male</option>
+                  <option value="gender-female">Female</option>
+                </select>
+                Country
+                <select name="select-country" id="">
+                  <option value="select-country">Select your country</option>
+                </select>
+                State
+                <select name="select-state" id="">
+                  <option value="select-state">Select your state</option>
+                </select>
+                Town
+                <select name="select-town" id="">
+                  <option value="select-town">Enter your town/city</option>
+                </select>
+              </div>
+              <div>
+                Profile Picture
+                <input type="file" name="Add-New" id="" placeholder="Add New" />
+              </div>
+            </div>
           </Tab>
           <Tab title="Password">
             Hello, these are password details
