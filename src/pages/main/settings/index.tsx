d886e0401342b15c0c40e4acfe3cfb0a501a23a4
Divine Philip <p.divine@truparse.com>
2021-02-17 08:15:46 +0100

    replaced lengthy code with components

--- src/pages/main/settings/index.tsx
+++ src/pages/main/settings/index.tsx
@@ -2,6 +2,10 @@ import { Button, Card, CustomInput, Tabs } from 'components';
 import React from 'react';
 import styles from './settings.module.scss';
 import Tab from '../../../components/tabs/tab'
+import BankDetails from './bank-details';
+import Verification from './verification';
+import Profile from './profile';
+import Password from './password';
 
 const Settings = () => {
   return (
@@ -10,61 +14,18 @@ const Settings = () => {
       <Card>
         <Tabs>
           <Tab title="Bank Details">
-            Account Number
-            <input type="text" />
-            Beneficiary Bank
-            <select name="" id="">
-              <option value="FirstBank">FirstBank</option>
-            </select>
-            Account name
-            <input type="text" />
-            <Button>Save</Button>
+            <BankDetails />
           </Tab>
           <Tab title="Verification">
-            Hello, these are verification details
-            {/* Contents of the tab */}
+            <Verification />
           </Tab>
           <Tab title='Profile'>
-            <div className={styles.profile}>
-              <div>
-                Name
-                <div>
-                  <input type="text" placeholder="First name" />
-                  <input type="text" placeholder="Last name" />
-                </div>
-                Date of Birth
-                <input type="date" name="date-of-birth" id="" />
-                Gender
-                <select name="select-gender" id="">
-                  <option value="select-gender">Select your gender</option>
-                  <option value="gender-male">Male</option>
-                  <option value="gender-female">Female</option>
-                </select>
-                Country
-                <select name="select-country" id="">
-                  <option value="select-country">Select your country</option>
-                </select>
-                State
-                <select name="select-state" id="">
-                  <option value="select-state">Select your state</option>
-                </select>
-                Town
-                <select name="select-town" id="">
-                  <option value="select-town">Enter your town/city</option>
-                </select>
-              </div>
-              <div>
-                Profile Picture
-                <input type="file" name="Add-New" id="" placeholder="Add New" />
-              </div>
-            </div>
+            <Profile />
           </Tab>
           <Tab title="Password">
-            Hello, these are password details
-            {/* Contents of the tab */}
+            <Password />
           </Tab>
         </Tabs>
-
       </Card>
     </>
   );
