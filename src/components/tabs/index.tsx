be38c437d1667463e3021961f9a60c0d8987608c
Divine Philip <p.divine@truparse.com>
2021-02-17 09:56:05 +0100

    updates to the settings page

--- src/components/tabs/index.tsx
+++ src/components/tabs/index.tsx
@@ -1,24 +1,29 @@
-import React, { ReactElement, useState } from 'react';
-import TabTitle from './tab-title';
-import styles from './tabs.module.scss'
+import React, { ReactElement, useState } from "react";
+import TabTitle from "./tab-title";
+import styles from "./tabs.module.scss";
 
 type Props = {
-  children: ReactElement[]
-}
+  children: ReactElement[];
+};
 
 const Tabs: React.FC<Props> = ({ children }) => {
-  const [selectedTab, setSelectedTab] = useState(0)
+  const [selectedTab, setSelectedTab] = useState(0);
 
   return (
     <div>
       <ul className={styles.tabs}>
         {children.map((item, index) => (
-          <TabTitle key={index} title={item.props.title} index={index} setSelectedTab={setSelectedTab} />
+          <TabTitle
+            key={index}
+            title={item.props.title}
+            index={index}
+            setSelectedTab={setSelectedTab}
+          />
         ))}
       </ul>
       {children[selectedTab]}
     </div>
-  )
-}
+  );
+};
 
-export default Tabs
+export default Tabs;
--- src/pages/main/settings/profile/index.tsx
+++ src/pages/main/settings/profile/index.tsx
@@ -1,44 +1,68 @@
-import React from 'react';
-import styles from './profile.module.scss';
+import { Button, CustomInput } from "components";
+import React from "react";
+import styles from "./profile.module.scss";
+import profile from "../../../../assets/img/profile.png";
 
 const Profile = () => {
   return (
-    <div>
-      <div className={styles.profile}>
-        <div>
-          Name
-                <div>
+    <div className={styles.profile}>
+      <div className="left-column">
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <div className={styles.name_grid}>
             <input type="text" placeholder="First name" />
             <input type="text" placeholder="Last name" />
           </div>
-                Date of Birth
-                <input type="date" name="date-of-birth" id="" />
-                Gender
-                <select name="select-gender" id="">
-            <option value="select-gender">Select your gender</option>
-            <option value="gender-male">Male</option>
-            <option value="gender-female">Female</option>
-          </select>
-                Country
-                <select name="select-country" id="">
-            <option value="select-country">Select your country</option>
-          </select>
-                State
-                <select name="select-state" id="">
-            <option value="select-state">Select your state</option>
-          </select>
-                Town
-                <select name="select-town" id="">
-            <option value="select-town">Enter your town/city</option>
+        </div>
+        <div className="form-group">
+          <label htmlFor="date_of_birth">Date of Birth</label>
+          <input type="date" name="date_of_birth" id="" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="gender">Gender</label>
+          <select name="select_gender" id="">
+            <option value="default">Select your gender</option>
+            <option value="gender_male">Male</option>
+            <option value="gender_female">Female</option>
           </select>
         </div>
-        <div>
-          Profile Picture
-                <input type="file" name="Add-New" id="" placeholder="Add New" />
+        <div className="form-group">
+          <label htmlFor="country">Country</label>
+          <select name="select_country" id="">
+            <option value="default">Select your country</option>
+            <option value="country_nigeria">Nigeria</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="state">State</label>
+          <select name="select_state" id="">
+            <option value="default">Select your state</option>
+            <option value="state_abia">Abia</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="town">Town</label>
+          <select name="select_town" id="">
+            <option value="default">Enter your town/city</option>
+            <option value="town">Aba</option>
+          </select>
+        </div>
+      </div>
+      <div className="right-column">
+        <div className="form-group">
+          <label htmlFor="profile_pic">Profile Picture</label>
+          <div className={styles.image_input}>
+            <div className={styles.image_box}>
+              <img src={profile} alt="" />
+            </div>
+            <div className={styles.button_box}>
+              <Button variant="outline">Add New</Button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
